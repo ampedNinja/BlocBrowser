8c673127ca4651560ed1992438a007962316d298
ejbeasley <ejbeasley@gmail.com>
2015-12-14 07:03:19 -0800

    Checkpoint 25 completed.

---
+++ BlocBrowser/AwesomeFloatingToolbar.m
@@ -0,0 +1,169 @@
+//
+//  AwesomeFloatingToolbar.m
+//  BlocBrowser
+//
+//  Created by eric on 12/13/15.
+//  Copyright © 2015 eric j beasley. All rights reserved.
+//
+
+#import "AwesomeFloatingToolbar.h"
+
+@interface AwesomeFloatingToolbar ()
+
+@property (nonatomic, strong) NSArray *currentTitles;
+@property (nonatomic, strong) NSArray *colors;
+@property (nonatomic, strong) NSArray *labels;
+@property (nonatomic, weak) UILabel *currentLabel;
+
+@end
+
+@implementation AwesomeFloatingToolbar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+- (instancetype) initWithFourTitles:(NSArray *)titles {
+    self = [super init];
+    
+    if (self) {
+        //Get the titles, set the colors
+        self.currentTitles = titles;
+        self.colors = @[[UIColor colorWithRed:199/255.0 green:158/255.0 blue:203/255.0 alpha:1],
+                        [UIColor colorWithRed:255/255.0 green:105/255.0 blue:97/255.0 alpha:1],
+                        [UIColor colorWithRed:222/255.0 green:165/255.0 blue:164/255.0 alpha:1],
+                        [UIColor colorWithRed:255/255.0 green:179/255.0 blue:71/255.0 alpha:1]];
+        
+        NSMutableArray *labelsArray = [[NSMutableArray alloc] init];
+        
+        // Make the 4 labels
+        for (NSString *currentTitle in self.currentTitles) {
+            UILabel *label = [[UILabel alloc] init];
+            label.userInteractionEnabled = NO;
+            label.alpha = 0.25;
+            
+            NSUInteger currentTitleIndex = [self.currentTitles indexOfObject:currentTitle];
+            NSString *titleForThisLabel = [self.currentTitles objectAtIndex:currentTitleIndex];
+            UIColor *colorForThisLabel = [self.colors objectAtIndex:currentTitleIndex];
+            
+            label.textAlignment = NSTextAlignmentCenter;
+            label.font = [UIFont systemFontOfSize:10];
+            label.text = titleForThisLabel;
+            label.backgroundColor = colorForThisLabel;
+            label.textColor = [UIColor whiteColor];
+            
+            [labelsArray addObject:label];
+        }
+        
+        self.labels = labelsArray;
+        
+        for (UILabel *thisLabel in self.labels) {
+            [self addSubview:thisLabel];
+        }
+    }
+    
+    return self;
+}
+
+- (void) layoutSubviews {
+    // Set the frames for the labels.
+    
+    for (UILabel *thisLabel in self.labels) {
+        NSUInteger currentLabelIndex = [self.labels indexOfObject:thisLabel];
+        
+        CGFloat labelHeight = CGRectGetHeight(self.bounds) / 2;
+        CGFloat labelWidth = CGRectGetWidth(self.bounds) / 2;
+        CGFloat labelX = 0;
+        CGFloat labelY = 0;
+        
+        // Adjust labelX and labelY for each label
+        if (currentLabelIndex < 2) {
+            // 0 or 1, go on top
+            labelY = 0;
+        } else {
+            // 2 or 3, go on bottom
+            labelY = CGRectGetHeight(self.bounds) / 2;
+        }
+        
+        if (currentLabelIndex % 2 == 0) {
+            //Is label divisable by 2? Goes on left.
+            labelX = 0;
+        } else {
+            // Otherwise, goes on right.
+            labelX = CGRectGetWidth(self.bounds) / 2;
+        }
+        
+        thisLabel.frame = CGRectMake(labelX, labelY, labelWidth, labelHeight);
+    }
+}
+
+#pragma mark - Touch Handling
+
+- (UILabel *)labelFromTouches:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:self];
+    UIView *subview = [self hitTest:location withEvent:event];
+    
+    if ([subview isKindOfClass:[UILabel class]]) {
+        return (UILabel *)subview;
+    } else {
+        return nil;
+    }
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UILabel *label = [self labelFromTouches:touches withEvent:event];
+    
+    self.currentLabel = label;
+    self.currentLabel.alpha = 0.5;
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    UILabel *label = [self labelFromTouches:touches withEvent:event];
+    
+    if (self.currentLabel != label) {
+        // The label being touched is not the initial label
+        self.currentLabel.alpha = 1;
+    } else {
+        // The label being touched is the initial label
+        self.currentLabel.alpha = 0.5;
+    }
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    UILabel *label = [self labelFromTouches:touches withEvent:event];
+    
+    if (self.currentLabel == label) {
+        NSLog(@"Label tapped: %@", self.currentLabel.text);
+        
+        if ([self.delegate respondsToSelector:@selector(floatingToolbar:didSelectButtonWithTitle:)]) {
+            [self.delegate floatingToolbar:self didSelectButtonWithTitle:self.currentLabel.text];
+        }
+    }
+    
+    self.currentLabel.alpha = 1;
+    self.currentLabel = nil;
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    self.currentLabel.alpha = 1;
+    self.currentLabel = nil;
+}
+
+#pragma mark - Button Enabling
+
+- (void)setEnabled:(BOOL)enabled forButtonWithTitle:(NSString *)title {
+    NSUInteger index = [self.currentTitles indexOfObject:title];
+    
+    if (index != NSNotFound) {
+        UILabel *label = [self.labels objectAtIndex:index];
+        label.userInteractionEnabled = enabled;
+        label.alpha = enabled ? 1.0 : 0.25;
+    }
+}
+
+@end
